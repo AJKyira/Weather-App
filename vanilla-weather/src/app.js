c159e383f0242a29b98cfc868b56bfb8cb4c3b09
AJKyira <117022446+AJKyira@users.noreply.github.com>
2022-12-18 12:28:05 +0000

    refined the city search

--- vanilla-weather/src/app.js
+++ vanilla-weather/src/app.js
@@ -28,17 +28,34 @@ function displayWeather(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  document.querySelector("#icon").innerHTML = iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let fahrenheitLink = document.querySelector("#units");
+  let temp = (fahrenheitLink.innerHTML * 9) / 5 + 32;
+  fahrenheitLink.innerHTML = Math.round(temp);
+}
+
+let fahrenheitElement = document.querySelector("#linkFahrenheit");
+fahrenheitElement.addEventListener("click", showFahrenheitTemp);
+
 function searchCity(city) {
-  let apiKey = "28td0ddb3a4b34da50o4f8354f47aeda";
-  let apiCall = `https://api.shecodes.io/weather/v1/current?query=${city}&appid=${apiCall}&units=metric`;
+  let apiKey = "405f120f098105bcacb069eb602acf8e";
+  let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiCall).then(displayWeather);
 }
 
@@ -49,65 +66,17 @@ function submit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "28td0ddb3a4b34da50o4f8354f47aeda";
-  let apiCall = `https://api.shecodes.io/weather/v1/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiCall}&units=metric`;
+  let apiKey = "405f120f098105bcacb069eb602acf8e";
+  let apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
   axios.get(apiCall).then(displayWeather);
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
 
-l;
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
+let searchForm = document.querySelector("#form");
+searchForm.addEventListener("submit", submit);
 
-  celsiusTemperature = response.data.main.temp;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-search("");
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = fahrenheitTemperature;
-}
-
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#linkFahrenheit");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#linkCelsius");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+searchCity("");
